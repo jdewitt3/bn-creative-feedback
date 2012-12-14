97bd950b9c8198311067b367a37b95458b8e951d
Joseph Spens <jspens@brandnetworksinc.com>
2012-12-14 00:52:59 -0500

    Added a 404 page

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php
+	$page_title = 'File Not Found';
+	require_once('include/header.php');
+?>
+<h1>You dun gone n broked my app</h1>
+<h2>Take another shot</h2>
+<?php require_once('include/footer.php'); ?>
--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,10 @@
+		</div>
 		<script src="js/plugins/jquery-1.8.3.min.js"></script>
         <script src="js/plugins/bootstrap.min.js"></script>
         <script src="js/utils.js"></script>
         <script src="js/script.js"></script>
+        <?php if(isset($page_script)){ ?>
         <script src="js/<?php echo $page_script; ?>"></script>
+       	<?php } ?>
     </body>
 </html>
